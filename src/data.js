0d60249f8335994335fccd882c497d15adc3ef69
Ali <alexandra.dedinska@tesena.com>
2023-05-29 11:14:16 +0200

    added status, ids and new commented query

--- src/data.js
+++ src/data.js
@@ -1,174 +1,217 @@
 export const books =
 [
     {
+    id:1,    
     author: {
             name: 'J.K. Rowling'
             },
     title: 'Harry Potter and the Philosophers Stone',
     year: 1997,
-    binding: 'PAPERBACK'
+    binding: 'PAPERBACK',
+    bookStatus : 'AVAILABLE'
     },
     {
+     id: 2,   
     author: {
             name: 'J.K. Rowling'
             },
     title: 'Harry Potter and the Chamber of Secrets',
     year: 1998,
-    binding: 'HARDCOVER'
+    binding: 'HARDCOVER',
+    bookStatus: 'UNAVAILABLE'
     },
     {
+    id: 3,    
     author: {
             name: 'George Orwell'
             },
     title: '1984',
     year: 1949,
     binding: 'PAPERBACK',
+    bookStatus: 'UNAVAILABLE'
     },
     {
+    id: 4,    
     author: {
             name: 'Harper Lee'
             },
     title: 'To Kill a Mockingbird',
     year: 1960,
-    binding: 'HARDCOVER'
+    binding: 'HARDCOVER',
+    bookStatus: 'AVAILABLE'
     },
     {
+    id: 5,    
     author: {
             name:'Markus Zusak'
             },
     title: 'The Book Thief',
     year: 2005,
-    binding: 'PAPERBACK'
+    binding: 'PAPERBACK',
+    bookStatus: 'UNAVAILABLE'
     },
     {
+    id:6,    
     author: {
             name: 'F. Scott Fitzgerald'
             },
     title: 'The Great Gatsby',
     year: 1925,
-    binding: 'HARDCOVER'
+    binding: 'HARDCOVER',
+    bookStatus: 'AVAILABLE'
     },
     {
+    id:7,    
     author: {
             name:'Ernest Hemingway'
             },
     title: 'The Old Man and the Sea',
     year: 1952,
-    binding: 'PAPERBACK'
+    binding: 'PAPERBACK',
+    bookStatus: 'AVAILABLE'
+
     },
     {
+    id: 8,    
     author: {
             name:'Jane Austen'
             },
     title: 'Sense and Sensibility',
     year: 1811,
-    binding: 'HARDCOVER'
+    binding: 'HARDCOVER',
+    bookStatus: 'AVAILABLE'
     },
     {
+    id: 9,    
     author: {
             name: 'J.R.R. Tolkien'
             },
     title: 'The Hobbit',
     year: 1937,
-    binding: 'PAPERBACK'
+    binding: 'PAPERBACK',
+    bookStatus: 'UNAVAILABLE'
     },
     {
+    id: 10,    
     author: {
             name: 'Agatha Christie'
             },
     title: 'Murder on the Orient Express',
     year: 1934,
-    binding: 'HARDCOVER'
+    binding: 'HARDCOVER',
+    bookStatus: 'AVAILABLE'
     },
     {
+    id: 11,    
     author: {
             name: 'Dan Brown'
             },
     title: 'The Da Vinci Code',
     year: 2003,
-    binding: 'PAPERBACK'
+    binding: 'PAPERBACK',
+    bookStatus: 'UNAVAILABLE'
     },
     {
+    id:12,    
     author: {
             name: 'Gabriel García Márquez'
             },
     title: 'One Hundred Years of Solitude',
     year: 1967,
-    binding: 'HARDCOVER'
+    binding: 'HARDCOVER',
+    bookStatus: 'AVAILABLE'
     },
     {
+    id:13,    
     author: {
             name: 'Emily Brontë'
             },
     title: 'Wuthering Heights',
     year: 1847,
-    binding: 'PAPERBACK'
+    binding: 'PAPERBACK',
+    bookStatus: 'AVAILABLE'
     },
     {
+    id: 14,    
     author:{
            name: 'Fyodor Dostoevsky'
            },
     title: 'Crime and Punishment',
     year: 1866,
-    binding: 'HARDCOVER'
+    binding: 'HARDCOVER',
+    bookStatus: 'AVAILABLE'
     },
     {
+    id: 15,    
     author: {
             name:'Hermann Hesse'
             },
     title: 'Siddhartha',
     year: 1922,
-    binding: 'PAPERBACK'
+    binding: 'PAPERBACK',
+    bookStatus:'AVAILABLE'    
     },
     {
+    id:16,    
     author: {
             name:'Haruki Murakami'
             },
     title: 'Norwegian Wood',
     year: 1987,
-    binding: 'HARDCOVER'
+    binding: 'HARDCOVER',
+    bookStatus: 'UNAVAILABLE'
     },
     {
+    id: 17,    
     author: {
             name: 'Virginia Woolf'
             },
     title: 'To the Lighthouse',
     year: 1927,
-    binding: 'PAPERBACK'
+    binding: 'PAPERBACK',
+    bookStatus: 'AVAILABLE'
     },
     {
+        id: 18,
     author: {
             name: 'Leo Tolstoy'
             },
     title: 'War and Peace',
     year: 1869,
-    binding: 'HARDCOVER'
+    binding: 'HARDCOVER',
+    bookStatus: 'UNAVAILABLE'
     },
     {
+    id:19,    
     author: {
             name:'Kazuo Ishiguro'
             },
     title: 'Never Let Me Go',
     year: 2005,
-    binding: 'PAPERBACK'
+    binding: 'PAPERBACK',
+    bookStatus:'UNAVAILABLE'
     },
     {
+    id:20,    
     author: {
             name: 'Albert Camus'
             },
     title: 'The Stranger',
     year: 1942,
-    binding: 'HARDCOVER'
+    binding: 'HARDCOVER',
+    bookStatus: 'AVAILABLE'
     },
     {
+    id:21,    
     author: {
             name: 'Gillian Flynn'
             },
     title: 'Gone Girl',
     year: 2012,
-    binding: 'PAPERBACK'
+    binding: 'PAPERBACK',
+    bookStatus:'UNAVAILABLE'
     }
-];
+]    
 
 
 //const authors = [{}]
